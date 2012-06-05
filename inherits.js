295b2f1a868cffffc629e31a662d12804f2ffb2a
Drew Young <drewyoung1@gmail.com>
2012-06-05 09:38:09 -0400

    Remove old files

--- inherits.js
+++
@@ -1,9 +0,0 @@
-var inherits = module.exports = function inherits(to, from) {
-	Object.getOwnPropertyNames(from.prototype).forEach(function(name) {
-		Object.defineProperty(to.prototype, name, Object.getOwnPropertyDescriptor(from.prototype, name))
-	});
-	
-	Object.defineProperty(to.prototype, 'constructor', {
-		value: to
-	});
-};
